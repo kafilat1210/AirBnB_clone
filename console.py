d3f3bc9bea818172d6b18588104c8414e53a48f1
kafilat1210 <cafylhearty@gmail.com>
2024-03-08 12:43:21 -0800

    built the console

---
+++ console.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""HBNBCommand module for the console"""
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """HBNB command processor"""
+
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
+        return True
+
+    def emptyline(self):
+        """An empty line + ENTER shouldn’t execute anything"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
